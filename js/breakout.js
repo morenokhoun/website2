cb593fa3238accd6e55e3f7f4cd9ba34cb76ff6b
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-03-21 23:26:37 +0000

    Thu, Mar 21, 2024, 4:26 PM -07:00

---
+++ js/breakout.js
@@ -0,0 +1,13 @@
+rulesBtn = document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
+
+
+// Rules open and close event handlers
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show')
+})
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
+})
