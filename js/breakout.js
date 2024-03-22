b314b1c4dc18f07a271b7734679c556f3169e0cc
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-03-22 20:58:50 +0000

    Fri, Mar 22, 2024, 1:58 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -6,6 +6,9 @@ ctx = canvas.getContext('2d')
 
 score = 0
 
+brickRowCount = 9
+brickColumnCount = 5
+
 // Create ball properties
 ball = {
     x: canvas.width / 2,
@@ -14,6 +17,30 @@ ball = {
     speed: 4,
     dx: 4;
     dy: -4;
+}
+
+// Create brick properties
+brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+// Create bricks
+bricks = []
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for (let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) +
+        brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) +
+        brickInfo.offsetY
+        bricks[i][j] = [x, y, ...brickInfo]
+    }
+
 }
 
 // Create Paddle properties
@@ -37,16 +64,104 @@ function drawPaddle () {
 
 // Draw score on canvas
 function drawScore() {
+    ctx.font = "20px Arial"
+    ctx.fillText(`Score: ${score},
+    canvas.width-100, 30)
+}
 
-    ctx.fillText(`Score: ${score}, canvas.width-100, 30)
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? '#0095dd'  : 'transparent';
+            ctx.fill
+            ctx.closePath()
+        })
+    })
 }
 
 // Draw everything
 function draw() {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     drawPaddle()
     drawBall()
     drawScore()
+    drawBricks()
 }
+
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x = paddle.x + paddle.dx
+
+    // wall detection
+    if (paddle.x < 0) {
+        paddle.x = 0
+    }
+    if (paddle.x + paddle.w > canvas.width){
+        paddle.x = canvas.width - paddle.w
+    }
+}
+
+
+//Keydown Event
+function keyDown(e) {
+    //console.log(e.key)
+    if (e.key == 'ArrowRight' || e.key == 'Right') {
+        paddle.dx = paddle.speed
+    }
+    if (e.key == 'ArrowLeft' || e.key == 'Left') {
+        paddle.dx = -paddle.speed
+    }
+}
+
+// Keyup Event
+function keyUp() {
+    if (e.key == 'ArrowRight' || e.key == 'Right'
+    || e.key == 'ArrowLeft'
+    || e.key == 'Left') {
+        paddle.dx = 0
+    }
+}
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown)
+document.addEventListener('keyup', keyUp)
+
+function moveBall() {
+    ball.x = ball.x + ball.dx
+    ball.y = ball.y + ball.dy
+
+    //wall collision (top)
+    if (ball.y + ball.size < 0) {
+        ball.dy = -1 * ball.dy
+    }
+
+    //wall collision (right)
+    if (ball.x + ball.size > canvas.width) {
+        ball.dx = -1 * ball.dx
+    }
+
+    //wall collision (bottom)
+    if (ball.y + ball.size > canvas.height) {
+        ball.dy = -1 * ball.dy
+    }
+
+    //wall collision (left)
+    if (ball.x + ball.size < 0){
+        ball.dx = -1 * ball.dx
+    }
+}
+
+// Update canvas drawing and animation
+function update() {
+    drawBall()
+    movePaddle()
+    draw()
+    requestAnimationFrame(updated)
+}
+
+update()
 
 draw()
 
