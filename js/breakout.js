e7ca0cb83262972220acc1a5d1b88fac38526580
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-03-22 00:26:32 +0000

    Thu, Mar 21, 2024, 5:26 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,57 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+score = 0
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+// Create Paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+// Draw paddle on canvas
+function drawPaddle () {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#009599'
+    ctx.fill()
+    ctx.closePath()
+}
+
+// Draw score on canvas
+function drawScore() {
+
+    ctx.fillText(`Score: ${score}, canvas.width-100, 30)
+}
+
+// Draw everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+}
+
+draw()
+
+drawPaddle()
+drawBall()
 
 
 // Rules open and close event handlers
