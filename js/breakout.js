330cf1a1b70fa968fa82f1ce7c0e6048bcb6aba7
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-04-02 20:10:25 +0000

    Tue, Apr 2, 2024, 12:10 PM -08:00

--- js/breakout.js
+++ js/breakout.js
@@ -145,12 +145,59 @@ function moveBall() {
     //wall collision (bottom)
     if (ball.y + ball.size > canvas.height) {
         ball.dy = -1 * ball.dy
+        showAllBricks()
+        score = 0
     }
 
     //wall collision (left)
     if (ball.x + ball.size < 0){
         ball.dx = -1 * ball.dx
     }
+
+    //paddle collision
+    if (ball.x - ball.size > paddle.x &&
+        ball.x + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y) {
+            ball.dy = -1 * ball.speed
+        }
+
+        //Brick collision
+        bricks.forEach(column => {
+            column.forEach(brick => {
+                if (brick.visible) {
+                    if (
+                        ball.x - ball.size > brick.x && //left brick side
+                        ball.x + ball.size < brick.x + brick.w && //right
+                        ball.y - ball.size < brick.y + brick.h //bottom)
+                        {
+                            ball.dy = -1 * ball.dy
+                            brick.visible = false
+                            increaseSscore()
+                        }
+
+                }
+            })
+        })
+
+}
+
+//increase score
+function increaseScore() {
+    score++ //score = score + 1
+
+    if (score == brickRowCount * brickColumnCount){
+        score = 0
+        showAllBricks()
+    }
+}
+
+function showAllBricks(){
+    brick.forEach(column => {
+        column.forEach(brick => {
+            brick.visible = true
+        })
+    })
+
 }
 
 // Update canvas drawing and animation
